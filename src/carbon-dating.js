1571a2d6541f0cc6b45f4b2d7c8b04de694d3fd8
Igor Pavetka <uropb.86@gmail.com>
2022-04-29 20:50:45 +0300

    made carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,28 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  let t = false;
+  if (typeof sampleActivity === 'string' || sampleActivity instanceof String) {
+    if (Number(sampleActivity)) {
+      newSambleActivity = Number(sampleActivity)
+    } else {
+      return false;
+    }
+    if ((newSambleActivity <=0)||(newSambleActivity >=15.0)) {
+      return false;
+    } else {
+      let k = 0.693/HALF_LIFE_PERIOD;
+      t = Math.ceil(Math.log(MODERN_ACTIVITY/newSambleActivity)/k);
+    }
+    
+  } else {
+    return false;
+  }
+  
+  return t;
+
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
